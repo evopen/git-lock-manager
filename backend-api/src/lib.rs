1239d88f6f6958b5f6649e7612eb94b958389d92
evopen <520dhh@gmail.com>
2020-10-24 13:11:00 +0800

    define APIs

--- backend-api/src/lib.rs
+++ backend-api/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub mod api {
+    use serde::{Deserialize, Serialize};
+    #[cfg(target_arch = "wasm32")]
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(tag = "cmd", rename_all = "camelCase")]
+    pub enum Request {
+        Echo { argument: String },
+        PickRepo,
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(tag = "cmd", rename_all = "camelCase")]
+    pub enum Request {
+        Echo { argument: String },
+        PickRepo { callback: String, error: String },
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    pub enum Response {
+        PickRepo { path: String },
     }
 }
